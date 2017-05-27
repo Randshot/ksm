b89579204ac91d69cb9917ccdfb7c4ca00a032c0
Ahmed Samy <asamy@protonmail.com>
2017-05-27 19:36:23 +0200

    bitmap: _BitScanForward returns index not nr, fix it

--- bitmap.h
+++ bitmap.h
@@ -74,7 +74,7 @@ static inline unsigned long __ffs(unsigned long x)
 #ifdef _MSC_VER
 	unsigned long i;
 	_BitScanForward(&i, x);
-	return i;
+	return i + 1;
 #else
 	return __builtin_ffs(x);
 #endif
@@ -85,7 +85,7 @@ static inline unsigned long __ffz(unsigned long x)
 #ifdef _MSC_VER
 	unsigned long i;
 	_BitScanForward(&i, ~x);
-	return i;
+	return i + 1;
 #else
 	return __builtin_ffs(~x);
 #endif
